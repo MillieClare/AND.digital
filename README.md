d413d4eee5f45714d9e23871591e1092724c58c6
Millie Clare <millie_clare@outlook.com>
2018-10-07 11:47:03 +0100

    Dropdown added, CSS styling

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
-# To-Do
+![Completed task screenshot](images/AND_task_final.png)
+
+## Completed
 - Split index.html into index.html, script.js & main.css
 - Use filter to remove candidates who don't list JS.
 - Check filter function when given other skills.
 - Create dropdown to filter within table.
 - Add CSS styling to tables.
+
+## To-Do
+- Nothing! :)
+
--- script.js
+++ script.js
@@ -32,19 +32,45 @@ function insertCandidate(tbody, name, skills) {
 function addCandidatesToTable(table, candidates) {
     candidates.forEach(candidate => insertCandidate(table, candidate.name, candidate.skills));
 }
-
+//function to filter by skill
 function filterCandidateBySkill(candidates, skill) {
     return candidates.filter(a => a.skills.includes(skill));
 }
 
-const candidatesTable = document.getElementById("candidates_example");
-const newCandidatesTable = candidatesTable.cloneNode(true);
+//function to create array consisting of one of each skill
+function createDropdownArray(candidates) {
+    const candidateArray = candidates.reduce((b, c) => [...b, ...c.skills], []);
+    return candidateArray.filter((a, i) => candidateArray.indexOf(a) === i);
+}
 
-removeRowsFromTable(newCandidatesTable);
-const newTbody = newCandidatesTable.getElementsByTagName('tbody')[0];
+//function to create dropdown, sorts skills alphabetically
+function createDropdown(skillsArray) {
+    let html = '<select id="dropdown"><option value="" selected="selected">Choose skill</option>';
+    for (let skill of skillsArray.sort()) {
+        html += `<option value='${skill}'>${skill}</option>`
+    }
+    html += '</select>';
+    document.getElementById('dropdownWrapper').innerHTML = html;
+}
 
-const filteredCandidates = filterCandidateBySkill(newCandidates, 'JavaScript')
-addCandidatesToTable(newTbody, filteredCandidates)
 
-document.body.appendChild(newCandidatesTable);
+window.addEventListener('load', () => {
+    //create copy of example table
+    const candidatesTable = document.getElementById("candidates_example");
+    const newCandidatesTable = candidatesTable.cloneNode(true);
+    //creates the dropdown with the list of skills
+    const dropdownArrayOptions = createDropdownArray(newCandidates);
+    createDropdown(dropdownArrayOptions);
 
+    //everytime a new selection is made, this function is executed.
+    document.getElementById('dropdown').addEventListener('change', event => {
+        //reset dropdown each time a different option is chosen. 
+        removeRowsFromTable(newCandidatesTable);
+        const newTbody = newCandidatesTable.getElementsByTagName('tbody')[0];
+        document.body.appendChild(newCandidatesTable);
+        //adapted function parameters to return the array of candidates with the skill selected by the drop down.
+        const filteredCandidates = filterCandidateBySkill(newCandidates, event.target.value);
+
+        addCandidatesToTable(newTbody, filteredCandidates)
+    })
+})
